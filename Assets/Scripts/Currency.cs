537ef7054b734cc1919d4bee0e93136a74ba2e43
BOWEN ZHANG <germanbook@hotmail.com>
2022-08-08 22:38:29 +1200

    Transferred UI data include currency between scenes PlayerLudus and TheArena

---
+++ Assets/Scripts/Currency.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Currency : MonoBehaviour
+{
+    public static Currency instance;
+    [SerializeField]private int currencyValue;
+    public Text currencyText;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        currencyValue = 100;
+    }
+
+    private void Update()
+    {
+        currencyText.text = Convert.ToString(currencyValue);
+    }
+
+    
+}
