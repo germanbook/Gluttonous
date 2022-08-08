20ee86ff140cc30401afece6c811b5ce08f705d6
DESKTOP-9J87B9N\Nikita - Work <nikitawingategarbovsky@gmail.com>
2022-08-09 10:24:41 +1200

    tried some work arounds to issues

--- Assets/Scripts/GladiatorStats/StatDisplay.cs
+++ Assets/Scripts/GladiatorStats/StatDisplay.cs
@@ -5,22 +5,22 @@ using TMPro;
 public class StatDisplay : MonoBehaviour
 {
     //Textmeshpro components from UI element (Change UI element Value to change Value)
-    TextMeshProUGUI GladiatorClassText;
-    TextMeshProUGUI HealthText;
-    TextMeshProUGUI ArmorTypeText;
-    TextMeshProUGUI WeaponTypeText;
+    public TextMeshProUGUI GladiatorClassText;
+    public TextMeshProUGUI HealthText;
+    public TextMeshProUGUI ArmorTypeText;
+    public TextMeshProUGUI WeaponTypeText;
 
     private void OnValidate()
     {
         //Grabs the text values from TMP components puts them in a list
         TextMeshProUGUI[] uiTexts = GetComponentsInChildren<TextMeshProUGUI>();
-        GladiatorClassText = uiTexts[0];
-        HealthText = uiTexts[1];
-        ArmorTypeText = uiTexts[2];
-        WeaponTypeText = uiTexts[3];
         
-
-        //to do: use these imported values into the list for future changes to stats.
+            GladiatorClassText = uiTexts[0];
+            HealthText = uiTexts[1];
+            ArmorTypeText = uiTexts[2];
+            WeaponTypeText = uiTexts[3];
+        
+            //to do: use these imported values into the list for future changes to stats.
     }
 }
 
