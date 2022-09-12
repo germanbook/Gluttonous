85c33cee992c8046bea76026db114e8f2d2a7a38
BOWEN ZHANG <germanbook@hotmail.com>
2022-09-12 22:05:08 +1200

    Start menu screen New Game & Exit Game done

---
+++ Assets/Scripts/GlobalGameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalGameManager : MonoBehaviour
+{
+    public GameObject startMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startMenu.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (startMenu.activeSelf == true)
+            {
+                startMenu.SetActive(false);
+            }
+            else
+            {
+                startMenu.SetActive(true);
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void NewGameButton()
+    {
+        SceneManager.LoadScene(1);
+        this.gameObject.SetActive(false);
+    }
+
+    public void LoadGameButton()
+    {
+        
+    }
+
+    public void ExitGameButton()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
