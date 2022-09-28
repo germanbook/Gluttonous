2983cb74264d732993fcadc34b42000be4a61119
BOWEN ZHANG <germanbook@hotmail.com>
2022-09-28 16:28:11 +1300

    Map UI mouse hover display done

---
+++ Assets/Scripts/MapButtonController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MapButtonController : MonoBehaviour, IPointerEnterHandler
+{
+    [SerializeField] GameObject panel;
+
+    private void Start()
+    {
+        panel.gameObject.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        panel.gameObject.SetActive(true);
+    }
+
+    public void OnPointerExit()
+    {
+        panel.gameObject.SetActive(false);
+    }
+
+}
