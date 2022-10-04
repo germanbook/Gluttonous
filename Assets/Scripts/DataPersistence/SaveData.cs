93eec8e97a7f9074f937e5c720f7c4226b8cd0fd
DESKTOP-9J87B9N\Nikita - Work <nikitawingategarbovsky@gmail.com>
2022-10-04 14:10:27 +1300

    save system completely finished

--- Assets/Scripts/DataPersistence/SaveData.cs
+++ Assets/Scripts/DataPersistence/SaveData.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 [System.Serializable]
 public class SaveData
 {
+    //The current count of each hired Gladiator
     public int SamniteCount;
     public int ThreaxCount;
     public int RetiariusCount;
     public int MurmilloCount;
+
+    //The current scene
+    public string CurrentScene;
+
+    //Tavern conversation system checks
+    public bool TavernDialogueHasPlayerInteractedBefore;
+    public bool TavernDialogueAdvancedQuestions;
+    public bool TavernDialogueIntroQuestions;
+
+    //Arena progression checks
+    public bool Arena2MapUnlocked;
+    public bool Arena3MapUnlocked;
+
+    //Demo experience checks
+    public bool LudusDemoExperienceFinished;
+    public bool ArenaDemoExperienceFinished;
+
 
     public SaveData (PlayerGladiatorsStore data)
     {
@@ -16,6 +34,18 @@ public class SaveData
         ThreaxCount = data.counterThraex;
         RetiariusCount = data.counterRetiarius;
         MurmilloCount = data.counterMyrmilo;
+
+        CurrentScene = data.currentScene;
+
+        TavernDialogueHasPlayerInteractedBefore = data.hasPlayerInteractedBefore;
+        TavernDialogueAdvancedQuestions = data.advancedQuestionsAnsweredBefore;
+        TavernDialogueIntroQuestions = data.introQuestionsAnsweredBefore;
+
+        Arena2MapUnlocked = data.arenaTwoUnlock;
+        Arena3MapUnlocked = data.arenaThreeUnlock;
+
+        LudusDemoExperienceFinished = data.ludusDemoExperienceFinished;
+        ArenaDemoExperienceFinished = data.arenaDemoExperienceFinished;
     }
 }
 
--- Assets/Scripts/NPC Scripts/PlayerGladiatorsStore.cs
+++ Assets/Scripts/NPC Scripts/PlayerGladiatorsStore.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerGladiatorsStore : MonoBehaviour
 {
+    //the count of the Gladiators the player has access to
     [HideInInspector] public int counterSamnites;
     [HideInInspector] public int counterThraex;
     [HideInInspector] public int counterMyrmilo;
     [HideInInspector] public int counterRetiarius;
+    //the current scene 
+    [HideInInspector] public string currentScene;
+
+    //progression of the game maps
+    [HideInInspector] public bool arenaTwoUnlock;
+    [HideInInspector] public bool arenaThreeUnlock;
+
+    //the demo experience checks
+    [HideInInspector] public bool ludusDemoExperienceFinished;
+    [HideInInspector] public bool arenaDemoExperienceFinished;
 
     [SerializeField] GladiatorStoreData gladiatorStoreData;
+
+    Scene scene;
 
     public bool hasPlayerInteractedBefore;
     public bool introQuestionsAnsweredBefore;
@@ -17,6 +31,7 @@ public class PlayerGladiatorsStore : MonoBehaviour
 
     private void Start()
     {
+        
         counterSamnites = gladiatorStoreData.counterSamnites;
         counterThraex = gladiatorStoreData.counterThraex;
         counterMyrmilo = gladiatorStoreData.counterMyrmilo;
@@ -34,13 +49,27 @@ public class PlayerGladiatorsStore : MonoBehaviour
         gladiatorStoreData.counterMyrmilo = counterMyrmilo;
         gladiatorStoreData.counterRetiarius = counterRetiarius;
     }
+
     //Utilises the SaveSystem class to save data
     //This function is used by the SaveButton
     public void SaveData()
     {
+        //Grabs the current scene and adds it to the currentScene variable
+        scene = SceneManager.GetActiveScene();
+        currentScene = scene.name;
+
+        //Arena progression unlocked bool's grabbed from GameManager
+        arenaTwoUnlock = GameManager.isArenaTwoUnlock;
+        arenaThreeUnlock = GameManager.isArenaThreeUnlock;
+
+        //Demo experience checks
+        ludusDemoExperienceFinished = GlobalGameManager.isDemoLudusFinished;
+        arenaDemoExperienceFinished = GlobalGameManager.isArenaOneDemoBattleFinished;
+
+        //saves the game data in this class 
         SaveSystem.SaveData(this);
     }
-    //
+    
     public void LoadData()
     {
         SaveData data = SaveSystem.LoadData();
@@ -49,5 +78,11 @@ public class PlayerGladiatorsStore : MonoBehaviour
         counterThraex = data.ThreaxCount;
         counterMyrmilo = data.MurmilloCount;
         counterRetiarius = data.RetiariusCount;
+        currentScene = data.CurrentScene;
+        hasPlayerInteractedBefore = data.TavernDialogueHasPlayerInteractedBefore;
+        introQuestionsAnsweredBefore = data.TavernDialogueIntroQuestions;
+        advancedQuestionsAnsweredBefore = data.TavernDialogueAdvancedQuestions;
+
+        SceneManager.LoadScene(currentScene);
     }
 }
